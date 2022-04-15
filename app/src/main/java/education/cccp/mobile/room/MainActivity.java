9b7bf0e615ae51a7047e83dd51ab93015a4cf727
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-04-15 09:54:24 +0200

    premier pas

---
+++ app/src/main/java/education/cccp/mobile/room/MainActivity.java
@@ -0,0 +1,16 @@
+package education.cccp.mobile.room;
+
+import static education.cccp.mobile.room.R.layout.activity_main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(activity_main);
+    }
+}
